f9a1fc8396e2ac5502aff3537cd5d4cf346b6195
Ben Fowler <bf118@canterbury.ac.uk>
2021-11-29 12:47:17 +0000

    Started Creating client side form Server side can be run locally rather than through make run

--- server/sample.py
+++ server/sample.py
@@ -17,3 +17,14 @@ class QuoteResource:
 
 app = falcon.App()
 app.add_route('/quote', QuoteResource())
+
+if __name__ == '__main__':
+
+
+    from wsgiref import simple_server
+    httpd = simple_server.make_server(['0.0.0.0'], ['8000'],create_wsgi_app (app))
+    try:
+        log.info('start')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
