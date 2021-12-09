14673642c86b8a8d12fbb3479d0914f524a3a1f7
Ben Fowler <bf118@canterbury.ac.uk>
2021-12-09 12:13:29 +0000

    Added JS to client side server is no in one file rather than across 2 Created file for the datastore finished client test makefile command

--- server/app.py
+++ server/app.py
@@ -1,23 +1,50 @@
 #python3 -m http.server 8000
 import falcon
 import json
-from views import getResource, postResource, deleResource
+from wsgiref.simple_server import make_server
 
 
 api = falcon.App()
-api.add_route('/get', getResource())
-api.add_route('/post', postResource())
-api.add_route('/delete', deleResource())
+class getResource:
+    def on_get(self,req,resp):
+        if req.get_param("id"):
+            resp.media = {'id' : "", 'lat' : "", 'lon' : "", 'description' : "", 'keywords' : ""}
+
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        pass
+
+class postResource:
+    def on_post(self,req,resp, **kwargs):
+
+       resp.media = {'id' : itemId, 'lat' : lat, 'lon' : lon, 'description' : description, 'keywords' : keywords}
+      
+       resp.status = falcon.HTTP_200
+       resp.content_type = falcon.MEDIA_JSON
+       pass
+
+class deleResource:
+    def on_del(self,req,resp):
+
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        pass
+
+get= getResource()
+post = postResource()
+delete = deleResource()
+
+api.add_route('/item', post)
+api.add_route('/items', get)
+api.add_route('/item/{itemId}',delete)
 # change to spec routing
 
 
 
 
 if __name__ == '__main__':
-    from wsgiref import simple_server
-    httpd = simple_server.make_server(['0.0.0.0'], [8000],(api))
-    try:
-        log.info('start')
+    with make_server('', 8000, api) as httpd:
+        print('Serving on port 8000...')
+
+        # Serve until process is killed
         httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
