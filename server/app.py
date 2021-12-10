b5292855a429490b6925c5059d7a4c01c63f13b2
Ben Fowler <bf118@canterbury.ac.uk>
2021-12-10 12:38:41 +0000

    Added single and multiple item get request methods

--- server/app.py
+++ server/app.py
@@ -4,43 +4,49 @@ import json
 from wsgiref.simple_server import make_server
 
 
+
 api = falcon.App()
-class getResource:
-    def on_get(self,req,resp):
-        if req.get_param("id"):
+class HTTPRequests():
+    
+    def on_get_single(self,req,resp):
+        
+        if req.param("id"):
             resp.media = {'id' : "", 'lat' : "", 'lon' : "", 'description' : "", 'keywords' : ""}
-
+        
         resp.status = falcon.HTTP_200
         resp.content_type = falcon.MEDIA_JSON
         pass
+    
+    def on_get_multiple(self,req, resp):
+        if req.param("id"):
+            resp.media
 
-class postResource:
-    def on_post(self,req,resp, **kwargs):
+            resp.status= falcon.HTTP_200
+            resp.content_type = falcon.MEDIA_JSON
+            pass
 
-       resp.media = {'id' : itemId, 'lat' : lat, 'lon' : lon, 'description' : description, 'keywords' : keywords}
+    def on_post(self,req,resp):
+        
+        resp.media = {'id' : itemId, 'lat' : lat, 'lon' : lon, 'description' : description, 'keywords' : keywords}
       
-       resp.status = falcon.HTTP_200
-       resp.content_type = falcon.MEDIA_JSON
-       pass
-
-class deleResource:
-    def on_del(self,req,resp):
-
         resp.status = falcon.HTTP_200
         resp.content_type = falcon.MEDIA_JSON
         pass
 
-get= getResource()
-post = postResource()
-delete = deleResource()
+    def on_del(self,req,resp):
+        
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        pass
 
-api.add_route('/item', post)
-api.add_route('/items', get)
-api.add_route('/item/{itemId}',delete)
+
+
+
+api.add_route('/item', HTTPRequests.on_post)
+api.add_route('/item/{itemId}',HTTPRequests.on_get_single)
+api.add_route('/items', HTTPRequests.on_get_multiple)
+api.add_route('/item/{itemId}',HTTPRequests.on_del)
 # change to spec routing
-
-
-
 
 if __name__ == '__main__':
     with make_server('', 8000, api) as httpd:
@@ -48,3 +54,6 @@ if __name__ == '__main__':
 
         # Serve until process is killed
         httpd.serve_forever()
+
+
+#remove classes for each one
