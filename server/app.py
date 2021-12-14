109d30b24d0c82e1b3697f1961a3d550daa81e3e
Ben Fowler <bf118@canterbury.ac.uk>
2021-12-14 12:42:48 +0000

    Updated post request and made additions to data store

--- server/app.py
+++ server/app.py
@@ -1,25 +1,32 @@
-#https://medium.com/codex/build-apis-with-falcon-in-python-all-essentials-you-need-9e2c2a5e1759
+#https://medium.com/codex/buid-apis-with-falcon-in-python-all-essentials-you-need-9e2c2a5e1759
+#https://careerkarma.com/blog/python-typeerror-list-indices-must-be-integers-or-slices-not-str/
 #http GET localhost:8000/item/{id}/
 #
 #python3 -m http.server 8000
 import falcon
 import json
+import datetime
 from wsgiref.simple_server import make_server
 from datastore import *
 
 class getResource():
     def on_get(self,req,resp ,userId,):
-        if req.params(id):
-            resp.media = json.dumps(example)
+        #if req.params(example["id"]):
+            resp.media = json.dumps(ITEMS)
         
             resp.status = falcon.HTTP_OK
             resp.content_type = falcon.MEDIA_JSON
-
+    
+    def on_delete(self,req,resp, userId):
+        
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        pass
 
 class getmultiResource():
    def on_get(self,req, resp):
         #if req.get_param("id"):
-            resp.media = example
+            resp.media = ITEMS
             
             resp.status= falcon.HTTP_200
             resp.content_type = falcon.MEDIA_JSON
@@ -28,26 +35,36 @@ class getmultiResource():
 class postResource():
     def on_post(self,req,resp):
         app = json.load(req.bounded_stream)
-        example.append(app)
-        resp.text= "example added"
-        resp.status = falcon.HTTP_201
+        
+        date_from = datetime.datetime.now()
+        date_to = datetime.datetime.now()
+        
+        fields =set({'user_id', 'keywords', 'description', 'lat', 'lon', 'date_from', 'date_to'})
+        if ITEMS.keys() != fields:
+            resp.status = falcon.HTTP_204
+        else:
+            app.update(ITEMS)
+            resp.text= "Data added"
+            resp.status = falcon.HTTP_201
+    #_id = max(ITEMS.keys()) + 1
+        
         resp.content_type = falcon.MEDIA_JSON
-    
-class deleResource():
-    def on_del(self,req,resp):
+
+
+api = falcon.App(middleware=falcon.CORSMiddleware(
+    allow_origins='http//:localhost:8001', allow_credentials='*'))
+
+class rootResource():
+    def on_get(self,req ,resp):
         
         resp.status = falcon.HTTP_200
-        resp.content_type = falcon.MEDIA_JSON
-        pass
 
 
 
-api = falcon.App()
-
+api.add_route('/', rootResource())
 api.add_route('/item', postResource())
 api.add_route('/item/{userId}/',getResource())
 api.add_route('/items', getmultiResource())
-api.add_route('/item/delete',deleResource())
 # change to spec routing
 
 if __name__ == '__main__':
@@ -56,5 +73,3 @@ if __name__ == '__main__':
 
         # Serve until process is killed
         httpd.serve_forever()
-
-
--- server/datastore.py
+++ server/datastore.py
@@ -1,20 +1,29 @@
-example = [
-{
-"id": 0,
-"user_id": "user1234",
-"keywords": [
-"hammer",
-"nails",
-"tools"
-],
+
+
+ITEMS ={"user_id": "user1234","keywords": ["hammer", "nails","tools"],
 "description": "A hammer and nails set",
-"image": "https://placekitten.com/200/300",
 "lat": 51.2798438,
 "lon": 1.0830275,
 "date_from": "2019-08-24T14:15:22Z",
-"date_to": "2019-08-24T14:15:22Z"
-}
-]
+"date_to": "2019-08-24T14:15:22Z"}
+
+class DataModelPythonDict:
+    def __init__(self, items):
+        self.items = items or {}
+        self.items_id_max = max(self.items.keys() or (0,0))
+    def get_item(self, id):
+        return self.items.get(id)
+    def delete_item(self, id):
+        del self.items[id]
+    def create_item(self, data):
+        self.items_id_max +=1
+        _id = self.items_id_max
+        self.items[_id] = data
+        data['id'] = _id
+        return data
+
+datastore = DataModelPythonDict(ITEMS)
+
 
 
 
